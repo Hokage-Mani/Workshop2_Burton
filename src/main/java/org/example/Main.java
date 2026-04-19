913cbc0ea2c9dd45f93a670fc9e274d8bc86def5
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-04-18 22:48:29 -0400

    At this point I had finally figured out how to complete my set of books in the library and make it to when a user inputs 1 that it displays all books that are available and not checked out.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,28 +8,29 @@ public class Main {
     public static void main(String[] args) {
         //You will only need ONE scanner
         Scanner scanner = new Scanner(System.in);
-        AvailableBooks.Book[] library = new AvailableBooks.Book[20];
+        Book[] iD = new Book[20];
 
-        library[0] = new AvailableBooks.Book("978-0062315007", "The Alchemist");
-        library[1] = new AvailableBooks.Book("978-1582405001", "Invincible");
-        library[2] = new AvailableBooks.Book("978-0765367302", "Halo: The Fall of Reach");
-        library[3] = new AvailableBooks.Book("978-0679456209", "My Life on the Road");
-        library[4] = new AvailableBooks.Book("978-0140268867", "The Odyssey");
-        library[5] = new AvailableBooks.Book("978-0451524935", "1984");
-        library[6] = new AvailableBooks.Book("978-0060935467", "To Kill a Mockingbird");
-        library[7] = new AvailableBooks.Book("978-0743273565", "The Great Gatsby");
-        library[8] = new AvailableBooks.Book("978-1503290563", "Pride and Prejudice");
-        library[9] = new AvailableBooks.Book("978-0316769174", "The Catcher in the Rye");
-        library[10] = new AvailableBooks.Book("978-0590353427", "Harry Potter and the Sorcerer's Stone");
-        library[11] = new AvailableBooks.Book("978-0544003415", "The Lord of the Rings");
-        library[12] = new AvailableBooks.Book("978-0441172719", "Dune");
-        library[13] = new AvailableBooks.Book("978-0345339683", "The Hobbit");
-        library[14] = new AvailableBooks.Book("978-1451673319", "Fahrenheit 451");
-        library[15] = new AvailableBooks.Book("978-0060850524", "Brave New World");
-        library[16] = new AvailableBooks.Book("978-0553418026", "The Martian");
-        library[17] = new AvailableBooks.Book("978-0593135204", "Project Hail Mary");
-        library[18] = new AvailableBooks.Book("978-0062316097", "Sapiens: A Brief History of Humankind");
-        library[19] = new AvailableBooks.Book("978-0399590504", "Educated");
+
+        iD[0] = new Book("978-0062315007", "The Alchemist");
+        iD[1] = new Book("978-1582405001", "Invincible");
+        iD[2] = new Book("978-0765367302", "Halo: The Fall of Reach");
+        iD[3] = new Book("978-0679456209", "My Life on the Road");
+        iD[4] = new Book("978-0140268867", "The Odyssey");
+        iD[5] = new Book("978-0451524935", "1984");
+        iD[6] = new Book("978-0060935467", "To Kill a Mockingbird");
+        iD[7] = new Book("978-0743273565", "The Great Gatsby");
+        iD[8] = new Book("978-1503290563", "Pride and Prejudice");
+        iD[9] = new Book("978-0316769174", "The Catcher in the Rye");
+        iD[10] = new Book("978-0590353427", "Harry Potter and the Sorcerer's Stone");
+        iD[11] = new Book("978-0544003415", "The Lord of the Rings");
+        iD[12] = new Book("978-0441172719", "Dune");
+        iD[13] = new Book("978-0345339683", "The Hobbit");
+        iD[14] = new Book("978-1451673319", "Fahrenheit 451");
+        iD[15] = new Book("978-0060850524", "Brave New World");
+        iD[16] = new Book("978-0553418026", "The Martian");
+        iD[17] = new Book("978-0593135204", "Project Hail Mary");
+        iD[18] = new Book("978-0062316097", "Sapiens: A Brief History of Humankind");
+        iD[19] = new Book("978-0399590504", "Educated");
 
         // Create 20 Book objects manually in an array
         //Or have ChatGPT help you
@@ -41,17 +42,30 @@ public class Main {
             //Use the scanner to take in the user input
             //For example
             System.out.println("\n=== BOOK STORE HOME SCREEN ===");
-
             System.out.println("1. Show Available Books");
-
             System.out.println("2. Show Checked Out Books");
-
             System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
 
+
             int userInput = scanner.nextInt();
-            if (userInput == 1);
-            System.out.println("Available Books: " + );
+            if (userInput == 1){
+                System.out.println("\n--- Available Books ---");
+                for(int i = 0; i < iD.length; i++){
+                    if (iD[i] != null){
+                        if(iD[i].isCheckedOut() == false){
+                            System.out.println("ID: " + i + " | Title: " + iD[i].getTitle());
+                        }
+                    }else if(userInput ==2){
+                        //for showing checked out books later
+                    }
+                }
+
+            }
+
+
+            //if(iD = 0)
+            //System.out.println("Available Books: " + );
 
             //Now use if statements depending on what the person typed
             //How many possibilities are there? Write your if statement structure first
